175d4bc951af2941c74310ed45220c19777d6937
odysseaspapadimas <odysseas.patra@gmail.com>
2021-08-17 21:14:50 +0300

    fixed bg color

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
         <style jsx global>{`
           body {
-            background: #0b0e11;
+            background-color: #0b0e11;
             color: #fff;
           }
         `}</style>
