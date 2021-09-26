553055307e2e74d7a8cb71e204a19e630b57995b
odysseaspapadimas <odysseas.patra@gmail.com>
2021-09-26 22:29:21 +0300

    Sort by date in places

--- components/Places/index.js
+++ components/Places/index.js
@@ -10,27 +10,37 @@ const Places = ({ seeOnMap, setSelectedPlace }) => {
     db.collection("users").doc(user.uid).collection("places")
   );
 
-  console.log(colError, colLoading, snapshot, 'snapshot');
+  console.log(colError, colLoading, snapshot, "snapshot");
   return (
     <div>
       {colLoading ? (
         <h1>Loading...</h1>
       ) : (
         <div className="space-y-5">
-          {snapshot.docs.map((doc) => (
-            <Place
-              key={doc.id}
-              docId={doc.id}
-              name={doc.data().name}
-              place={doc.data().place}
-              desc={doc.data().desc}
-              date={doc.data().date}
-              coords={doc.data().coords}
-              seeOnMap={seeOnMap}
-              setSelectedPlace={setSelectedPlace}
-              userUid={user.uid}
-            />
-          ))}
+          {snapshot.docs
+            .sort((a, b) => {
+              if (!b.data().date?.seconds) {
+                return -1;
+              } else if (!a.data().date?.seconds) {
+                return 1;
+              } else {
+                return b.data().date.seconds - a.data().date.seconds;
+              }
+            })
+            .map((doc) => (
+              <Place
+                key={doc.id}
+                docId={doc.id}
+                name={doc.data().name}
+                place={doc.data().place}
+                desc={doc.data().desc}
+                date={doc.data().date}
+                coords={doc.data().coords}
+                seeOnMap={seeOnMap}
+                setSelectedPlace={setSelectedPlace}
+                userUid={user.uid}
+              />
+            ))}
         </div>
       )}
     </div>
